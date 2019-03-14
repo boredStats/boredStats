d32d39922ada39c54657537fff38d65bbad9fc6b
boredStats <ian.abenes@gmail.com>
2019-03-14 16:55:47 -0500

    revamped pls_tools
    Changelog:
    Fixed procrustes rotation, documentation
    Fixed original multitable plsc

--- boredStats/pls_tools.py
+++ boredStats/pls_tools.py
@@ -44,13 +44,13 @@ class MultitablePLSC(object):
         num_vars_in_y = y.shape[1]
         num_vars_in_x = [x.shape[1] for x in x_list]
         
-        cross_xy = np.ndarray(shape=[sum(num_vars_in_x), num_vars_in_y])
+        cross_xy = np.ndarray(shape=[num_vars_in_y, sum(num_vars_in_x)])
         
+        start_index = 0
         for x_index, x_table in enumerate(x_list):
-            cross_corrmat = cross_corr(x_table, y)
-            start = num_vars_in_x[x_index] * x_index
-            end = num_vars_in_x[x_index] * (x_index + 1)
-            cross_xy[start:end, :] = cross_corrmat
+            cross_corrmat = cross_corr(y, x_table)
+            end_index = start_index + num_vars_in_x[x_index]
+            cross_xy[:, start_index:end_index] = cross_corrmat
             
         return cross_xy
     
@@ -62,7 +62,7 @@ class MultitablePLSC(object):
             - axis rotation (change in order of components)
             - axis reflection (change in sign of loadings)
             
-        See McIntosh & Lobaugh, 2004 for more
+        See McIntosh & Lobaugh, 2004 'Assessment of significance'
         
         Parameters:
         -----------
@@ -74,65 +74,61 @@ class MultitablePLSC(object):
         
         Returns:
         --------
-        rot_u : rotated left singular values
-        rot_s : rotated diagonal matrix
-        rot_v : rotated right singular values
+        rotated_u : rotated left singular values
+        rotated_diag : rotated diagonal matrix values
+        rotated_v : rotated right singular values
         """
         
-        ov = orig_svd(0)
-        rv = resamp_svd(0)
-        rs = resamp_svd(1)
-        ru = resamm_svd(2)
+        original_v = orig_svd[2]
+        perm_u = resamp_svd[0]
+        perm_diag = resamp_svd[1]
+        perm_v = resamp_svd[2]
 
-        n, _, p = np.svd(np.matmul(ov.T, rv))
+        n, _, p = np.linalg.svd(np.dot(original_v.T, perm_v), full_matrices=False)
+        rotation_matrix = n.dot(p.T)
         
-        q = np.matmul(n, p.T)
+        rotated_u = np.dot(np.dot(perm_u, np.diagflat(perm_diag)), rotation_matrix)
+        rotated_v = np.dot(rotation_matrix, np.dot(np.diagflat(perm_diag), perm_v))
         
-        rot_u = ru * rs * q
-        rot_v = rv * rs * q
+        sum_of_squares_rotated_u = np.sum(rotated_v[:, :]**2, 0)
+        rotated_diag = np.sqrt(sum_of_squares_rotated_u)
         
-        ss_rot_v = np.sum(rot_v**2, 0) #sum of squares of rotated v
-        rot_s = np.sqrt(ss_rot_v)
-        
-        return rot_u, rot_s, rot_v
+        return rotated_u, rotated_diag, rotated_v
     
     @staticmethod
     def p_from_perm_mat(obs_vect, perm_array):
-    """Calculate p-values columnwise
-    
-    Parameters:
-    -----------
-    obs_vect : numpy array
-    Vector of true observations
-    
-    perm_array : numpy array
-    N x M array of observations obtained through permutation
-        N is the number of permutations used
-        M is the number of variables
-    
-    Returns:
-    --------
-    p_values : numpy array
-    Vector of p-values corresponding to obs_vect
-    """
+        """Calculate p-values columnwise
 
-    n_iters = perm_array.shape[0]
-    p_values = np.ndarray(shape=obs_vect.shape)
-    for t, true in enumerate(obs_vect):
-        perm_data = perm_array[t, :]
-        p_values[t] = utils.permutation_p(true, perm_data, n_iters)
-    return p_values
-   
-    @staticmethod
-    def mult_plsc(y_table, x_tables):
+        Parameters:
+        -----------
+        obs_vect : numpy array
+        Vector of true observations
+
+        perm_array : numpy array
+        N x M array of observations obtained through permutation
+            N is the number of permutations used
+            M is the number of variables
+
+        Returns:
+        --------
+        p_values : numpy array
+        Vector of p-values corresponding to obs_vect
+        """
+        p_values = np.ndarray(shape=obs_vect.shape)
+        for t, true in enumerate(obs_vect):
+            perm_data = perm_array[:, t]
+            p_values[t] = utils.permutation_p(true, perm_data)
+        return p_values
+    
+    def mult_plsc(self, y_table, x_tables):
         """Calculate multitable PLS-C, fixed effect model
         See Krishnan et al., 2011 for more
         """
         corr_xy = self.build_corr_xy(y_table, x_tables)
         centered_corr_xy = utils.center_matrix(corr_xy)
         
-        u, delta, v = np.linalg.svd(centered_corr_xy)
-        return u, delta, v.T
+        u, delta, v = np.linalg.svd(centered_corr_xy, full_matrices=False)
+        return u, delta, v
 
     def perm_mult_plsc(self, y_table, x_tables):
         """Run permutation based testing to determine
@@ -148,12 +144,10 @@ class MultitablePLSC(object):
         
         Returns:
         --------
-        p_values : numpy array
-        Array of p_values corresponding to the singular values
-        
-        perm_singular_array : numpy array (optional)
-        N x M array of singular values obtained by permuting the
-        rows in each table; self.return_perm must be True
+        output : dict
+        Dictionary of outputs
+            Defaults to original eigenvalues and associated p-values
+            return_perm=True includes matrix of permutation eigenvalues
         
         See Krishnan et al., 2011, 'Deciding which latent variables to keep' 
         """
@@ -161,27 +155,29 @@ class MultitablePLSC(object):
             return AttributeError("Number of permutations cannot be None")
         
         orig_svd = self.mult_plsc(y_table, x_tables)
-        orig_sing = orig_svd(1)
+        orig_sing = orig_svd[1]
         
         n = 0
         perm_singular_values = np.ndarray(shape=[self.n_iters, len(orig_sing)])
         while n != self.n_iters:
+            #print('Working on iteration %d out of %d' % (int(n+1), self.n_iters))
             perm_y = utils.perm_matrix(y_table)
-            perm_x_tables = [perm_matrix(x) for x in x_tables]
+            perm_x_tables = [utils.perm_matrix(x) for x in x_tables]
             
-            perm_xy = self.build_corr_xy(perm_y, perm_x_tables)
-            centered_perm_xy = utils.center_matrix(perm_xy)
-            perm_svd = np.linalg.svd(centered_perm_xy)
-            
+            perm_svd = self.mult_plsc(perm_y, perm_x_tables)
             rot_perm = self.procrustes_rotation(orig_svd, perm_svd)
-            perm_singular_values[n, :] = rot_perm
+
+            perm_singular_values[n, :] = rot_perm[1]
+            n += 1
         
-        p_values = utils.p_from_perm_mat(orig_sing, perm_singular_values)
+        p_values = self.p_from_perm_mat(orig_sing, perm_singular_values)
         
+        output = {'true_eigenvalues' : orig_sing,
+                 'p_values' : p_values}
         if self.return_perm:
-            return p_values, perm_singular_values
-        else:
-            return p_values
+            output['permutation_eigs'] = perm_singular_values
+        
+        return output
     
     def bootstrap_saliences(self, y_table, x_tables, z_tester=2):
         """Run bootstrap testing on saliences
@@ -205,8 +201,8 @@ class MultitablePLSC(object):
             raise AttributeError("Number of iterations cannot be None")
             
         orig_svd = self.mult_plsc(y_table, x_tables)
-        orig_y_sals = orig_svd(0)
-        orig_x_sals = orig_svd(2)
+        orig_y_sals = orig_svd[0]
+        orig_x_sals = orig_svd[2]
         
         n = 0
         perm_y_sals = np.ndarray(shape=[orig_y_sals.shape, self.n_iters])
@@ -217,8 +213,8 @@ class MultitablePLSC(object):
             
             resamp_svd = self.mult_plsc(resamp_y, resamp_x_list)
             rot_svd = self.procrustes_rotation(orig_svd, resamp_svd)
-            perm_y_sals[:, :, n] = rot_svd(0)
-            perm_x_sals[:, :, n] = rot_svd(2)
+            perm_y_sals[:, :, n] = rot_svd[0]
+            perm_x_sals[:, :, n] = rot_svd[2]
         
         perm_y_sals_std = np.std(perm_y_sals, axis=-1)
         perm_x_sals_std = np.std(perm_x_sals, axis=-1)

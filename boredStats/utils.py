8994cf49b9366cfcbc3899b9caae8df4953b8051
boredStats <ian.abenes@gmail.com>
2019-03-15 13:14:44 -0500

    removed dependency for statsmodels
    Currently only requires statsmodels for fdr-correction of p-values, which can be done on the fly. For now I'm removing it.

--- boredStats/utils.py
+++ boredStats/utils.py
@@ -7,7 +7,7 @@ Created on Thu Mar  7 10:37:27 2019
 
 import numpy as np
 from numpy.random import permutation as pf
-from statsmodels.stats import multitest as mt
+#from statsmodels.stats import multitest as mt
 
 def center_matrix(a):
     """
@@ -24,13 +24,13 @@ def perm_matrix(matrix):
     """
     return np.asarray([pf(matrix[:, col]) for col in range(matrix.shape[1])]).T
 
-def fdr_pmatrix(p_matrix):
-    """
-    Apply a FDR correction to a matrix of p-values
-    """
-    pvect = np.ndarray.flatten(p_matrix)
-    _, fdr_p = mt.fdrcorrection(pvect)
-    return np.reshape(fdr_p, p_matrix.shape)
+#def fdr_pmatrix(p_matrix):
+#    """
+#    Apply a FDR correction to a matrix of p-values
+#    """
+#    pvect = np.ndarray.flatten(p_matrix)
+#    _, fdr_p = mt.fdrcorrection(pvect)
+#    return np.reshape(fdr_p, p_matrix.shape)
 
 def permutation_p(observed, perm_array):
     #see Phipson & Smyth 2010 for more information
